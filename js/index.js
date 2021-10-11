cc969d959304777cb43f1cd328e7db8fb55a102d
Christian Madufor <chrismadufor@gmail.com>
2021-10-11 22:10:34 +0100

    add slider to hero section

--- js/index.js
+++ js/index.js
@@ -6,3 +6,22 @@ function goToCart() {
     window.location.assign('cart.html')
 
 }
+
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:2
+    //     },
+    //     1000:{
+    //         items:3
+    //     }
+    // }
+})
