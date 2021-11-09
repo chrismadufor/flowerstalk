d90fdab16579dc1aa95590f0fd4aeb126cffb791
Christian Madufor <chrismadufor@gmail.com>
2021-11-09 07:05:53 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -7,21 +7,64 @@ function goToCart() {
 
 }
 
-$('.owl-carousel').owlCarousel({
-    items: 1,
-    loop:true,
-    margin:10,
-    nav:true,
-    autoplay: true,
-    // responsive:{
-    //     0:{
-    //         items:1
-    //     },
-    //     600:{
-    //         items:2
-    //     },
-    //     1000:{
-    //         items:3
-    //     }
-    // }
-})
+
+function showMobileNav() {
+    const mobileNav = document.querySelector('.mobile-nav')
+    if (mobileNav.classList.contains('show-nav')) {
+        mobileNav.classList.remove('show-nav')
+        document.querySelector('body').classList.add('lock-scroll')
+    }
+        
+    else {
+        mobileNav.classList.add('show-nav')
+        document.querySelector('body').classList.remove('lock-scroll')
+    }
+}
+
+window.onclick = function(e) {
+    let modal = document.querySelector('.modal')
+    if (e.target == modal) {
+        closeModal()
+    }
+}
+
+function closeModal() {
+    let modal = document.querySelector('.modal')
+    if (modal.style.display == 'none')
+        modal.style.display = 'flex'
+    else modal.style.display = 'none'
+}
+
+function pickDeliveryMethod() {
+    let pickUp = document.querySelector('#pickup')
+    let delivery = document.querySelector('#delivery')
+    let pickupInputs = document.querySelector('.pickup-inputs')
+    let deliveryInputs = document.querySelector('.delivery-inputs')
+
+    if(window.innerWidth > 500) {
+        if(pickUp.checked) {
+            pickupInputs.classList.remove('hide-inputs')
+            deliveryInputs.classList.add('hide-inputs')
+        }
+        if(delivery.checked) {
+            pickupInputs.classList.add('hide-inputs')
+            deliveryInputs.classList.remove('hide-inputs')
+        }
+    }else {
+        if(pickUp.checked) {
+            pickupInputs.classList.remove('hide-inputs')
+            deliveryInputs.classList.add('hide-inputs')
+        }
+        if(delivery.checked) {
+            pickupInputs.classList.add('hide-inputs')
+            deliveryInputs.classList.remove('hide-inputs')
+        }
+    }
+}
+
+
+
+
+
+
+
